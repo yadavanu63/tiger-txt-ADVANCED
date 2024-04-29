6d3de07308757e1ebd6e080cad2114f78c35d975
MR. YADAV <163430575+yadavanu63@users.noreply.github.com>
2024-04-29 16:46:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,4 @@
-import os
-
-API_ID = API_ID = 22609670
-
-API_HASH = os.environ.get("API_HASH", "a98b5be0127986be1cc2553dbd99765e")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7139260466:AAEHwNQrZnGb8fzuQhNxEvlAl6Ud5_T_ddU")
-
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-OWNER = int(os.environ.get("OWNER", 7118905960))
-
-LOG = -1002005269251
-
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6127347210").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
-
-
+api_id = "24478182"
+api_hash = "a98b5be0127986be1cc2553dbd99765e"
+bot_token = "7088632591:AAEvdJBXacrQYmGqEUc1mpzdegX_5wRumw8"
+auth_groups = "-1002068352431"
